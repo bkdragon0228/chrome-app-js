d4ee678a69c0f3ca6717b8b7af77140c8f85e6b1
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-28 15:02:18 +0900

    새로고침을 막는 방법

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 "use strict";
 
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  console.log(loginInput.value); // input의 값을 얻는 방법
+function onLoginSubmit(info) {
+  // event에 호출된 함수에 들어가는 매개변수, 이벤트에 대한 부분이 저장되어있다.
+  info.preventDefault(); // 새로고침을 막음(브라우저의 기본동작을 막는다..)
+  console.log(info);
   const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    alert("your name is too long");
-  } // html 에서 해결함!
+  console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+// 제출이 일어나면 함수를 실행
+// 허나 새로고침이 일어나는 문제가 발생(기본으로 그렇게 되어있는 구조)
