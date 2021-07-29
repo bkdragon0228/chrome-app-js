9c0e15e1512afc1df27e0cc2254e0cc760e4cbd7
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-29 13:44:46 +0900

    시계를 페이지에 보이게 함

--- JS/clock.js
+++ JS/clock.js
@@ -1,12 +1,9 @@
 const clock = document.querySelector("#clock");
 
 function getClock() {
-  const date = new Date(); // Date 객체 생성
-  console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-// setInterval(sayHello, 5000); // 매 5초
-// setTimeout(sayHello, 5000); // 5초 뒤 한번
-
-getClock(); // 한번 실행 후
-setInterval(getClock, 1000); // 매초마다 실행
+getClock();
+setInterval(getClock, 1000);
