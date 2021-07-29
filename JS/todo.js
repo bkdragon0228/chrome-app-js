b7136efe4de1d997464473d9fe21750311040067
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-29 15:46:15 +0900

    todo 리스트 삭제하기 추가

--- JS/todo.js
+++ JS/todo.js
@@ -1,22 +1,31 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-// todo-form 안의 input을 찾아옴
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  //   console.log(event.target.parentElement); // 뭘 눌렀는지 찾기
+  const delLi = event.target.parentElement;
+  delLi.remove(); // 삭제~!
+}
 function paintToDo(newToDo) {
   const list = document.createElement("li");
   const span = document.createElement("span");
-  list.appendChild(span);
+  const btn = document.createElement("button");
 
   span.innerText = newToDo;
-  //   console.log(list); // 확인용
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+
+  list.appendChild(span);
+  list.appendChild(btn);
+
   toDoList.appendChild(list);
 }
 
 function handleToDoSubmit(event) {
-  event.preventDefault(); // 새로 고침 막기
-  const newToDo = toDoInput.value; // 다른 변수에 저장해두고
-  toDoInput.value = ""; // 값을 지운다.
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
   paintToDo(newToDo);
 }
 
