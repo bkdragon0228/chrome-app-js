16342c4eccaaf4ef904a69ffd8c7f05d22be8252
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-29 16:02:36 +0900

    todo list localstorage에 저장

--- JS/todo.js
+++ JS/todo.js
@@ -2,10 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = []; // 배열 생성, localstarage 저장용
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+  // 배열을 string 형태로 localstorage에 저장
+}
+
 function deleteToDo(event) {
-  //   console.log(event.target.parentElement); // 뭘 눌렀는지 찾기
   const delLi = event.target.parentElement;
-  delLi.remove(); // 삭제~!
+  delLi.remove();
 }
 function paintToDo(newToDo) {
   const list = document.createElement("li");
@@ -26,7 +32,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo); //  배열에 저장
   paintToDo(newToDo);
+  saveToDos(); // 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
