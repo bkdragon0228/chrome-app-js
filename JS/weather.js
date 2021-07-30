1994fe2d6121f1c7172545643ffa3d162edbf6c6
bkdragon0228 <rlaqjarb2067@naver.com>
2021-07-30 17:28:56 +0900

    날씨, 위치, 온도 추가

--- JS/weather.js
+++ JS/weather.js
@@ -1,11 +1,21 @@
+const API_KEY = "72e72f6fe76e93fe2b0851a1c8865614";
+
 function onGeoOk(position) {
-  // 객체를 유일한 매개변수로 받는 콜백 함수
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`you live in ${lat}, ${lng}`);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      const weatherContainer = document.querySelector(
+        "#weather span:first-child"
+      );
+      const cityContainer = document.querySelector("#weather span:last-child");
+      cityContainer.innerText = data.name;
+      weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
+  ); // 개발자 도구 network에 뜬다.
 }
 function onGeoError() {
-  // error 났을 때 함수
   alert("Can't find you. No  weather for you");
 }
 
